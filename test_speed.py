90189f940c93b86ea7ea8555442a12d093d6fdef
Ecohnoch <542305306@qq.com>
2019-04-02 14:14:41 +0800

    TFRecords finished

--- test_speed.py
+++ test_speed.py
@@ -6,6 +6,7 @@ import time
 import threading
 
 from utils.my_utils import load_wave_list
+from utils.my_utils import load_data
 from thin_resnet import resnet34
 
 BATCH_SIZE = 128
@@ -149,11 +150,73 @@ def multi_thread_test():
         coord.join()
 
 
-def generate_tfrecord():
-	pass
+def generate_tfrecord(output_path):
+    train_file_list, train_label_list = audio_data_extracted(SPLIT_FILE, WAV_DIR)
+    writer = tf.python_io.TFRecordWriter(os.path.join(output_path, 'tran.tfrecords'))
+    for ind, (file, label) in enumerate(zip(train_file_list, train_label_list)):
+        audio = load_data(file)
+        audio_raw = audio.tobytes()
+        label = int(label)
+        example = tf.train.Example(features=tf.train.Features(feature={
+                'audio_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[audio_raw])),
+                'label':     tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))
+            }))
+        writer.write(example.SerializeToString())
+        if ind % 1000 == 0:
+            print("%d num audios processed" % ind)
+        if ind % 25000== 0:
+            print("25000 num processed")
+            break
+    writer.close()
 
-def tfrecord_test():
-	pass
+def parse_function(example_proto):
+    features = {
+        'audio_raw': tf.FixedLenFeature([], tf.string),
+        'label':     tf.FixedLenFeature([], tf.int64)
+    }
+    features = tf.parse_single_example(example_proto, features)
+    audio_file = tf.decode_raw(features['audio_raw'], tf.float32)
+    audio_file = tf.reshape(audio_file, [257, 250, 1])
+    label      = tf.cast(features['label'], tf.int64)
+
+    return audio_file, label
+
+def tfrecord_test(trans_file):
+    dataset = tf.data.TFRecordDataset(trans_file)
+    dataset = dataset.map(parse_function)
+    dataset = dataset.shuffle(buffer_size=10000)
+    dataset = dataset.batch(BATCH_SIZE)
+    iterator= dataset.make_initializable_iterator()
+    next_element = iterator.get_next()
+
+    x = PLACE_HOLDER['thin_resnet'][0]
+    y = PLACE_HOLDER['thin_resnet'][1]
+
+    emb = resnet34(x)
+
+
+    config = tf.ConfigProto()
+    config.allow_soft_placement = True
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+
+        sess.run(iterator.initializer)
+
+        batch_10_start = time.time()
+        for i in range(10):
+            start_time = time.time()
+            batch_train, batch_train_label = sess.run(next_element)
+
+            batch_start_time = time.time()
+            emb_val = sess.run(emb, feed_dict={x: batch_train})
+            batch_end_time   = time.time()
+            print('Counter:', i, 'Step time:', batch_end_time - start_time, 'Batch time:', batch_end_time - batch_start_time)
+        batch_10_end   = time.time()
+        print('10 Batch time consumed: ', batch_10_end - batch_10_start)
+
 
 
 
@@ -161,6 +224,8 @@ def tfrecord_test():
 
 
 if __name__ == '__main__':
-	multi_thread_test()
+	# multi_thread_test()
+    generate_tfrecord('/data/ChuyuanXiong/up/audio_trans')
+    # tfrecord_test('/data/ChuyuanXiong/up/audio_trans/tran.tfrecords')
 
 
